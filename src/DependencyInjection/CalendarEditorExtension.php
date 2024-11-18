bc7bb23586ebf2f1a2606218b129b0abe24054d6
Steffen Winde <inserv@winde-ganzig.de>
2024-11-18 09:23:54 +0100

    typo fehler edit

--- src/DependencyInjection/CalendarEditorExtension.php
+++ src/DependencyInjection/CalendarEditorExtension.php
@@ -25,6 +25,7 @@ class CalendarEditorExtension extends Extension
 	public function load(array $configs, ContainerBuilder $container): void
 	{
 		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
-		$loader->load('services.yaml');
+
+		$loader->load('services.yml');
 	}
 }
